00cd9e0267d70571aef5d3c38acee316809a19c4
Isaj011 <yole.maneahk@gmail.com>
2020-06-12 23:21:16 +0300

    Commit for folio

---
+++ src/containers/Main.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Header from "../components/header/Header";
+import Greeting from "./greeting/Greeting";
+import Skills from "./skills/Skills";
+import StackProgress from "./skillProgress/skillProgress";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
+import Top from "./topbutton/Top";
+import Profile from "./profile/Profile";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Greeting />
+        <Skills />
+        <StackProgress />
+        <Projects />
+        <Profile />
+        <Top />
+      </div>
+    );
+  }
+}
---
+++ src/portfolio.js
@@ -0,0 +1,131 @@
+﻿
+/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  username: "Saad Ismael",
+  title: "Hi all, I'm Saad",
+  subTitle: emoji("A passionate Full Stack Software Developer 🚀 creative thinker who is adept at coming up with real solutions that work for clients. With experience in a wide variety of software, system architectures and programming languages, I am always current with the latest developments in the software development world"),
+  resumeLink: ""
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+
+  github: "https://github.com/Isaj011",
+  gmail: "ismaelsaad011@gmail.com"
+  
+};
+
+// Your Skills Section
+
+const skillsSection = {
+  title: "What i do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji("⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ Integration of third party services such as Firebase/ AWS ")
+  ],
+
+/* Make Sure You include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Programming",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Frontend/Design",  //Insert stack or technology you have experience in
+      progressPercentage: "75%"  //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    },
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: "Your Github Converted Token",
+  githubUserName: "Your Github Username",
+  showGithubProfile :"true" // Set true or false to show Contact profile using Github, defaults to false 
+};
+
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle: "My Inbox is open for all.",
+  number:"" ,
+  email_address: "ismaelsaad011@gmail.com"
+};
+
+//Twitter Section
+
+const twitterDetails = {
+
+  userName : "twitter"//Replace "twitter" with your twitter username without @
+  
+};
+export { greeting, socialMediaLinks, skillsSection, techStack, openSource, contactInfo , twitterDetails};
